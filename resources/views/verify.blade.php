fea34775434bcd17380799a792ea29065d586062
Brenda Ruto <66879198+WebBree@users.noreply.github.com>
2022-12-18 17:57:37 +0100

    Email functionality
    In .env file

---
+++ resources/views/verify.blade.php
@@ -0,0 +1,10 @@
+@extends('main')
+@section('content')
+    <div class="verify-msg">
+
+    <p><i class="fas fa-badge-check"></i>You have registered successfully. Please check your email for a verification link.</p>
+    <a href=""><i class="fab fa-yahoo"></i></a>
+    <a href=""><i class="fab fa-google"></i></a>
+
+    </div>
+@endsection
