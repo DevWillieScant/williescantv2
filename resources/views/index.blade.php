9c3d79763ac9180084686df6cd9f1eaa5217448e
Brenda Ruto <66879198+WebBree@users.noreply.github.com>
2022-12-24 21:08:32 +0300

    Home page changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
         <img src="{{url('images/logo.png')}}" alt="Not Found" class="logo2">
         <div class="search">
             <form action="">
-                <input type="text" placeholder="Search by your product name">
-                <button type="submit">Search <i class="fas fa-search"></i></button>
+                <input type="text" placeholder="Search">
+                <button type="submit"><i class="fas fa-search"></i></button>
             </form> 
         </div>
     </div>  
